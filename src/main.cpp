323f7611729389802b92f969057ff0bbfa07fc8c
Anjas Ade <133207917+javacode084@users.noreply.github.com>
2023-08-29 20:52:49 +0800

    Update Firmware

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "code/maintask.h"
+#include "main_app.h"
 // #include "code/LDR_PID.h"
 // #include "code/LDR.h"
 // #include "code/LDR_MOTOR.h"
@@ -14,7 +15,7 @@
 // #include "code/soilRTC.h"
 
 //GABUNGAN:
-// 1. Pembacaan LDR timur,barat,selatan,utara
+// 1. Pembacaan LDR timur,barat,selatan,utara dengan PID kontrol
 // 2. Pembacaan sensor tegangan dan arus untuk pompa_SensorA(0x40)
 // 3. Setting Pompa sesuai RTC dan kelembaban tanah
 //    Jadwal On: 17.00 selama 30 menit(On progress)
